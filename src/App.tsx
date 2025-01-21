6f928abbd881ad495699b4a3f52c36e4e59aead7
kimhyunji <rlaguswl221@icloud.com>
2025-01-21 20:13:03 +0900

    feat: add router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import Header from './components/layout/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 }
