026e1ac665bf7550329b72cda31d36a94ff739c8
kimhyunji <rlaguswl221@icloud.com>
2025-01-23 17:08:28 +0900

    feat: Update Tailwind CSS config and add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        myongji: "#002e66",
+        myongji: '#002e66',
+        gray: {
+          custom: '#767676',
+        },
       },
     },
   },
