492c9df51aec176497797433729dd73f942bcf9e
kimhyunji <rlaguswl221@icloud.com>
2025-01-23 17:19:17 +0900

    style: update header height

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import logo from '@/assets/logo.png';
 import AuthButtons from '@/components/common/AuthButtons';
 
-export default function Header() {
+const Header = () => {
   return (
     <>
       <div className="w-full h-32 bg-myongji shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] flex">
@@ -10,9 +10,10 @@ export default function Header() {
           <img className="h-16" src={logo} alt="명지대학교" />
         </Link>
       </div>
-      <div className="w-full h-11 flex justify-end items-center  bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+      <div className="w-full h-14 flex justify-end items-center  bg-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
         <AuthButtons />
       </div>
     </>
   );
-}
+};
+export default Header;
