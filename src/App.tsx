cf5d81fd979ddacbbde9a06e23d30b9e767fe5ce
kimhyunji <rlaguswl221@icloud.com>
2025-02-21 12:52:25 +0900

    feat: add React Query

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/components/layout/Header';
 import Layout from '@/components/layout/Layout';
 import Footer from '@/components/layout/Footer';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <Layout>
-        <Outlet />
-      </Layout>
-      <Footer />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <Layout>
+          <Outlet />
+        </Layout>
+        <Footer />
+      </div>
+    </QueryClientProvider>
   );
 }
