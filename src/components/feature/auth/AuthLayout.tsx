084238f9db9b509cc91c3178b1d6f03071df2f9c
kimhyunji <rlaguswl221@icloud.com>
2025-02-21 15:51:13 +0900

    style: adjust margin-top value

--- src/components/feature/auth/AuthLayout.tsx
+++ src/components/feature/auth/AuthLayout.tsx
@@ -10,7 +10,7 @@ const AuthLayout = ({ children, title }: AuthLayoutProps) => {
   return (
     <div className="flex flex-col items-center">
       <PageTitle>{title}</PageTitle>
-      <div className="w-full max-w-[520px] min-h-[400px] p-20 mt-4 bg-white rounded-[50px] shadow-lg">
+      <div className="w-full max-w-[520px] min-h-[400px] p-20 mt-6 bg-white rounded-[50px] shadow-lg">
         {children}
       </div>
     </div>
