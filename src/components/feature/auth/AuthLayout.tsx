9759f97d3243def22f520ee46a9f84177ba7c333
kimhyunji <rlaguswl221@icloud.com>
2025-08-28 21:15:32 +0900

    feat: add responsive padding to AuthLayout for mobile

--- src/components/feature/auth/AuthLayout.tsx
+++ src/components/feature/auth/AuthLayout.tsx
@@ -8,9 +8,9 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children, title }: AuthLayoutProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center ">
       <PageTitle>{title}</PageTitle>
-      <div className="w-full max-w-[520px] min-h-[400px] p-20 mt-6 bg-white rounded-[50px] shadow-lg">
+      <div className="w-full max-w-[520px] min-h-[400px] p-10 sm:p-12 md:p-16 lg:p-20 mt-6 bg-white rounded-[20px] sm:rounded-[50px] shadow-lg">
         {children}
       </div>
     </div>
